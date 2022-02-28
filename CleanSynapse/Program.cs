ce53a64a3fd903bece6bee8ec51424f63591555a
gen3vra <git@gen.work>
2022-02-28 00:19:54 -0800

    old source release

---
+++ CleanSynapse/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows.Forms;
+using Razer.Emily.Common;
+using Razer.Emily.UI;
+//using Razer.Emily.UISDK;
+using Razer.Storage;
+
+class Program
+{
+    #region Hiding from taskbar
+    [DllImport("user32.dll")]
+    static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+    [DllImport("user32.dll")]
+    static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    [DllImport("user32.dll")]
+    static extern IntPtr GetShellWindow();
+    internal const UInt32 SC_CLOSE = 0xF060;
+    internal const UInt32 MF_GRAYED = 0x00000001;
+    #endregion
+    static string defaultDirectory = @"C:\Program Files (x86)\Razer\Synapse";
+    static void Main(string[] args)
+    {
+        #region Hide program from taskbar
+        IntPtr current = Process.GetCurrentProcess().MainWindowHandle;
+        EnableMenuItem(GetSystemMenu(current, false), SC_CLOSE, MF_GRAYED);
+        IntPtr shellWin = GetShellWindow();
+        SetParent(current, shellWin);
+        #endregion
+        if (File.Exists(AssemblyLocation(true) + "\\RzSynapse.exe"))
+        {
+            MainStorage mainApp = Singleton<MainStorage>.Instance;
+            mainApp.Initialize(Storage_Project.Emily);
+            if (!mainApp.TryLastLogin())
+                MessageBox.Show("Please login on Razer Synapse first.");
+            else
+            {
+                RzDeviceManager dManager = new RzDeviceManager();
+                dManager.Enumerate();
+                CommonConfigLoader configLoader = Singleton<CommonConfigLoader>.Instance;
+                configLoader.StartConfig();
+                RzDevice[] allDevices = dManager.ActiveDevices.ToArray();
+                foreach (RzDevice rzDevice in allDevices)
+                {
+                    rzDevice.RefreshData();
+                    configLoader.DeviceAdded(rzDevice.VID, rzDevice.PID);
+                    //PluginDevice pluginDevice = configLoader.FindDevice(rzDevice.PID);
+                    //Console.WriteLine("Loaded: " + pluginDevice.Name);
+                }
+            }
+        }
+        else
+        {
+            DialogResult result = DialogResult.None;
+            if (args.Length < 1)
+            {
+                if (Directory.Exists(defaultDirectory))
+                    result = MessageBox.Show("The program is not in the Synapse folder. Automatically move it?", "Synapse Was Not Found", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                else
+                {
+                    MessageBox.Show("The program is not in the Synapse folder, and we can't find the directory. You probably installed it somewhere else. Please manually move the program to the root of the Razer Synapse directory.");
+                    return;
+                }
+            }
+            if (result == DialogResult.Yes || args.Length > 0)
+            {
+                WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+                bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+                if (!hasAdministrativeRight)
+                {
+                    string fileName = Assembly.GetExecutingAssembly().Location;
+                    ProcessStartInfo processInfo = new ProcessStartInfo
+                    {
+                        Verb = "runas",
+                        Arguments = "-true",
+                        FileName = fileName
+                    };
+                    try
+                    {
+                        Process.Start(processInfo);
+                    }
+                    catch
+                    {
+                        //Thrown if cancelled.
+                        MessageBox.Show("The program needs admin to move the file to your program files.");
+                        MessageBox.Show("The program will now exit.");
+                        return;
+                    }
+                    return;
+                }
+                else
+                {
+                    string path = defaultDirectory + "\\" + Path.GetFileName(AssemblyLocation(false));
+                    File.Copy(AssemblyLocation(false), path, true);
+                    Process.Start(path);
+                    result = MessageBox.Show("Run program at computer startup? Please disable Synapse from running at startup if yes.", "Add Startup Shortcut", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                    if (result == DialogResult.Yes)
+                    {
+                        AddShortcut("CleanSynapse", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup");
+                    }
+                    MessageBox.Show("Completed installation.");
+                    return;
+                }
+            }
+            if (result == DialogResult.No)
+            {
+                MessageBox.Show("The program will now exit.");
+                return;
+            }
+        }
+    }
+
+    public static string AssemblyLocation(bool directory)
+    {
+        string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+        UriBuilder uri = new UriBuilder(codeBase);
+        string path = Uri.UnescapeDataString(uri.Path);
+        return directory ? Path.GetDirectoryName(path) : path;
+    }
+
+    private static void AddShortcut(string linkName, string dir)
+    {
+        string deskDir = dir;
+        try
+        {
+            File.Delete(dir);
+        }
+        catch { }
+        using (StreamWriter writer = new StreamWriter(deskDir + "\\" + linkName + ".url"))
+        {
+            string app = Assembly.GetExecutingAssembly().Location;
+            writer.WriteLine("[InternetShortcut]");
+            writer.WriteLine("URL=file:///" + app);
+            writer.WriteLine("IconIndex=0");
+            string icon = app.Replace('\\', '/');
+            writer.WriteLine("IconFile=" + icon);
+            writer.Flush();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# CleanSynapse ![No Synapse](https://i.imgur.com/wNP4zOl.png)
+<img alt="Maintenance" src="https://img.shields.io/maintenance/yes/2018">
+
+Removes the need to run Synapse in the background to use your custom profiles for Razer peripherals.
+
+# What does this do?
+It automatically starts the service required to run your custom profiles for your Razer peripherals. In other words, you can have custom keyboard effects, (custom lighting, waves, whatever) without having to run Synapse in the background. Best part is, it doesn't even run itself in the background. It starts the service and then exits.
+
+Of course, if you want to run Synapse later, you can. It does not interfere with normal operation. If you quit Synapse and your profiles disable, you can simply run the program again.
+
+# How do I acquire this illustrious piece of tech?
+Click the "Releases" tab, download the .exe, and run it. If you installed your Razer Synapse to somewhere other than the default, please manually put the .exe in the root of that directory.
+
+# Should I run this at login?
+I would. If you ran the program normally and it installed it for you, it should have presented an option to start at login. In that case, it placed a shortcut in *"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"*. If you installed it manually, you will need to create a shortcut and place it here as well. You will be able to see it in the Task Manager's startup tab after a reboot if you did it correctly.
+![help](https://i.imgur.com/Uh30kjR.png)
+![help](https://i.imgur.com/8mbnpVt.png)
+
+## Make sure to disable Razer Synapse from running at login as well.
+- Open task manager.
+- Click on the "startup" tab.
+- Click on Razer Synapse.
+- If enabled, click "Disable".
+![help](https://i.imgur.com/D8yYC1e.png)
+
+# Limitations 
+Unfortunately, keybinds that you've set in Synapse will NOT work. I cannot control this without setting up my program to ALSO run in the background exactly like Synapse would anyway. If you want keybinds, might as well just run Synapse. If you're just looking for custom colors, use this.
